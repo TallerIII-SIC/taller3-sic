a430aa77ebdaa2579fded01d9d4d5b4109f1527f
marcos <marcospernica@gmail.com>
2018-12-17 01:00:34 -0300

    Eliminadas mediciones NTP4

--- AnalisisNTP4/graficos.py
+++
@@ -1,9 +0,0 @@
-from nucleo import calcularFda
-
-def graficarFda(datos, plt, etiqueta, longitud):
-	fda = calcularFda(datos)
-	longitudFda = len(fda)
-	x = fda[:]
-	y = [x/longitudFda for x in range(longitudFda)]
-	plt.plot(x, y, label = etiqueta)
-	
