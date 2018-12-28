f3fe01fa90b8bb5952d318773629f3c1d9f5296f
eze210 <eze210@gmail.com>
2018-12-28 00:20:36 -0300

    Now allan function receives the TE in seconds.

--- src/allan.py
+++ src/allan.py
@@ -1,15 +1,18 @@
 import allantools
 import matplotlib.pyplot as plt
+import numpy as np
 
 
+# Receives the TE in seconds and plots the Allan TDEV
 def plotTDEV(TE):
-    a = allantools.Dataset(data=TE)
+    frequency = np.diff(TE)
+    a = allantools.Dataset(data=frequency, data_type='freq')
     a.compute("tdev")
 
     plotter = allantools.Plot()
     plotter.plot(a, errorbars=True, grid=True)
 
     plotter.ax.set_xlabel("Tau [s]")
-    plotter.ax.set_ylabel("TDEV [us]")
+    plotter.ax.set_ylabel("TDEV [s]")
     plt.title("Varianza de Allan para SIC")
     plotter.show()
